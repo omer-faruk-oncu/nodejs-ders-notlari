99d2eb5f49d5ea095348bddb1649ab4ee6e32e4d
Omer <farukoncu78@gmail.com>
2024-07-04 14:24:52 +0300

    regex

--- express/app.js
+++ express/app.js
@@ -1,29 +1,37 @@
 'use strict'
 
-
+/*---------------*/
 const express = require('express')
 const app = express()
+/*---------------*/
+
+// app.get('/', (req, res) => { res.end('app.get çalıştı')})
+// app.post('/', (req, res) => { res.end('app.post çalıştı')})
+// app.put('/', (req, res) => { res.end('app.put çalıştı')})
+// app.delete('/', (req, res) => { res.end('app.delete çalıştı')})
+//app.all('/', (req, res) => { res.end('app.all çalıştı')})
+
+// app.route('/')
+//     .get((req, res) => { res.end('app.route get çalıştı')})
+//     .post((req, res) => { res.end('app.route post çalıştı')})
+//     .put((req, res) => { res.end('app.route put çalıştı')})
+//     .delete((req, res) => { res.end('app.route delete çalıştı')})
+
+//app.get('/path', (req, res) => { res.send('burası path')})
+
+app.get(/^\/xyz/, (req, res) => { res.send('now in here: /xyz$/')})
+
+
+/*---------------*/
+/*---------------*/
 
 require('dotenv').config()
-
-
 const PORT = process.env.PORT || 8000
-
 app.listen(PORT, () => {
     console.log('Running: http://127.0.0.1' + PORT)
 })
+/*---------------*/
 
-app.get('/', (req,res)=>{
-    //res.end('app.get çalıstı')
 
-    const obj = {
-        error:false,
-        message:'welcome'
-    }
-
-    //res.end(JSON.stringify(obj))
-    res.send(obj)
-
-})
 
 
