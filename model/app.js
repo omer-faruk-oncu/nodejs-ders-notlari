6d97877f90a80ef7b48f47c8f1485bbe07b281e8
Omer <farukoncu78@gmail.com>
2024-07-10 09:17:01 +0300

    up

--- model/app.js
+++ model/app.js
@@ -11,24 +11,83 @@ const PORT = process.env.PORT || 8000;
 
 /* ------------------------------------------------------- */
 // Accept json data:
-app.use(express.json())
+app.use(express.json());
 
-app.all('/', (req, res) => {
-    res.send('WELCOME TO TODO API')
-})
+// app.all("/", (req, res) => {
+//   res.send("WELCOME TO TODO API");
+// });
 
 // continue from here...
+/* ------------------------------------------------------- */
+
+const { Sequelize, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize(
+  "sqlite:" + process.env.SQLITE || "./db.sqlite3"
+);
+
+const Todo = sequelize.define("todos", {
+  title: {
+    type: DataTypes.STRING(256),
+    allowNull: false,
+  },
+
+  description: DataTypes.TEXT,
+  priority: {
+    type: DataTypes.TINYINT,
+    allowNull: false,
+    defaultValue: 0,
+  },
+  isDone: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+});
+
+//sequelize.sync({ alter: true });
+
+sequelize
+  .authenticate()
+  .then(() => console.log("*DB connected"))
+  .catch(() => console.log( "*DB not connected"));
+
+/* ------------------------------------------------------- */
+
+const router = express.Router()
+
+router.post('/', (req,res)=>{
+
+    const receivedData = req.body
+    console.log(receivedData)
+
+
+})
+
+app.use(router)
+
+
+
+
+
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
 
 const errorHandler = (err, req, res, next) => {
-    const errorStatusCode = res.errorStatusCode ?? 500
-    console.log('errorHandler worked.')
-    res.status(errorStatusCode).send({
-        error: true, // special data
-        message: err.message, // error string message
-        cause: err.cause, // error option cause
-        // stack: err.stack, // error details
-    })
-}
-app.use(errorHandler)
+  const errorStatusCode = res.errorStatusCode ?? 500;
+  console.log("errorHandler worked.");
+  res.status(errorStatusCode).send({
+    error: true, // special data
+    message: err.message, // error string message
+    cause: err.cause, // error option cause
+    // stack: err.stack, // error details
+  });
+};
+app.use(errorHandler);
 /* ------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
