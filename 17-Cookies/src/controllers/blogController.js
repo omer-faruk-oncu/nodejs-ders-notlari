6574cce5fcf722d3c55881dfb396f97229f38058
Omer <farukoncu78@gmail.com>
2024-07-25 10:07:35 +0300

    up

--- 17-Cookies/src/controllers/blogController.js
+++ 17-Cookies/src/controllers/blogController.js
@@ -98,35 +98,9 @@ module.exports.blogPost = {
 
     list: async (req, res) => {
 
-        //console.log(req.query)
-
-        const filter = req.query?.filter || {}
-
-        const search = req.query?.search
-        //console.log(search)
-
-        for(let key in search)
-            search[key] = {$regex: search[key] }
-
-        const sort = req.query?.sort || {}
-        //console.log(sort)
-      
-
-        let limit = Number(req.query?.limit )
-
-        limit = limit > 0 ? limit :  Number(process.env?.PAGE_SIZE || 20)
-        //console.log(limit, typeof limit)
-
-
-        let page = (req.query?.page)
-        page = page > 0 ? page : 1 
-
-        let skip= Number(req.query?.skip)
-        skip = skip > 0 ? skip : (page-1) * limit 
-
-
-        const data = await BlogPost.find({...filter, ...search}).sort(sort).skip(skip).limit(limit).populate('categoryId')
-
+    
+   
+       
         res.status(200).send({
             error: false,
             result: data
