cf400d1f3c9ffbcbeab5c1482f4006a128516cd1
Omer <farukoncu78@gmail.com>
2024-08-03 12:06:24 +0300

    starter rent a car

--- 25-RentAcar/_projectStarter/src/middlewares/permissions.js
+++ 25-RentAcar/_projectStarter/src/middlewares/permissions.js
@@ -1,28 +1,33 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
 // Middleware: permissions
 
 module.exports = {
+  isLogin: (req, res, next) => {
+    if (req.user) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You must login.");
+    }
+  },
 
-    isLogin: (req, res, next) => {
-
-        if (req.user) {
-            next()
-        } else {
-            res.errorStatusCode = 403
-            throw new Error('NoPermission: You must login.')
-        }
-    },
-
-    isAdmin: (req, res, next) => {
-
-        if (req.user && req.user.isAdmin) {
-            next()
-        } else {
-            res.errorStatusCode = 403
-            throw new Error('NoPermission: You must login and to be Admin.')
-        }
-    },
-}
+  isAdmin: (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You must login and to be Admin.");
+    }
+  },
+  isStaffOrisAdmin: (req, res, next) => {
+    if (req.user && (req.user.isStaff || req.user.isAdmin)) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You must be staff or an Admin.");
+    }
+  },
+};
